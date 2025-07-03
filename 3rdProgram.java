28b13f839cba5da0770ef46674f5fb57e70b15fe
srinivas <addanki.srinivas03@gmail.com>
2025-07-03 10:23:00 +0530

    3304

---
+++ 3rdProgram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char kthCharacter(int k) {
+        StringBuilder sri=new StringBuilder("a");
+        while(sri.length()<k){
+            int n=sri.length();
+            for(int i=0;i<n;i++){
+                sri.append((char)('a'+(sri.charAt(i)-'a'+1)%26));   // it's little bit confusing i need to understand it.
+            }
+        }
+        return sri.charAt(k-1);
+        
+    }
+}
